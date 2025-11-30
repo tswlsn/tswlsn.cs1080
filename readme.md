34fe27bd547da922078da7e8715e461fbe288651
tswlsn <166852467+tswlsn@users.noreply.github.com>
2025-11-30 15:57:31 -0500

    Add files via upload

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
-This is a totally awesome website and it is proud of it.
-Final project for CS1080.
-Majority of criteria satisfied on "Say hi!" and "Reviews" pages.
+Project Title: Self-conscious website
+A website that is very proud of itself to a fault, attempting to prevent the user from leaving and fixing reviews if poor ones are left.
+
+Team members: Thomas W. Role: everything
+
+Features:
+-Four pages, all linked to each other. Responsive design that folds header into columns on narrow screens, and uses flexboxes.
+-Dark mode toggle, present in bottom left of page
+-Custom cursor (Functions as normal on all pages except "Say hi!", in which case the height is modified to add to the illusion of the site controlling the cursor. Tragically it is impossible to actually control the cursor, but we as users of the internet should be grateful for this.)
+-HTML Form with JS validation (Requires an alphanumeric name and uses a default if none is provided, must include text in the text area which can be any character, but will remove angle brackets < > to prevent tampering)
+-Very nice looking CSS if I do say so myself. Beautiful background gradient in both light and dark mode, text always legible. Shows/hides a popup on the "Say hi!" page, which appears if the cursor attempts to exit via the top of the page.
+-localStorage used to store reviews, read upon entering page to display.
+-JavaScript present in droves: Used for the custom cursor, controlling the cursor, dark mode toggle, and review page. Handles click events. Review page uses DOM manipulation to add new reviews, and will add new reviews on its own if the calculated review score is below 4.
--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let reviews = JSON.parse(localStorage.getItem("reviews")) || [];
 let avgReview = 0;
 let dark = false;
 
-function toggleDarkMode() {
+function toggleDarkMode() { //toggles dark mode
 	let a = "#252525";
 	let b = "#eeeeee";
 	if (dark == true) {a = "#eeeeee"; b = "#252525"; dark = false}
@@ -19,62 +19,60 @@ function toggleDarkMode() {
 	document.documentElement.style.setProperty('--bgcol', b);
 }
 
-if (url.includes("review.html")) {
-updateReviews();
-function postReview() {
-	let revName = document.getElementById("username").value;
-	if (revName == "") {revName = "anonymous"}
-	let revText = document.getElementById("review").value
-	revText = revText.replaceAll("<", "");
-	revText = revText.replaceAll(">", "");
-	let revVal = document.querySelector('input[name="rad"]:checked').value;
-	let revTime = new Date();
-	let postmark = revName + " at " + revTime.toLocaleDateString() + " " + revTime.toLocaleTimeString();
-	reviews.push([postmark, revText, revVal]);
-	localStorage.setItem("reviews", JSON.stringify(reviews));
-}
-
-function updateReviews() {
-	let revHTML = "";
-	for (let i = 0; i < reviews.length; i++) {
-		let star = "";
-		for (let j = 0; j < reviews[i][2]; j++) {star += "★";}
-		for (let j = 0; j < 5 - reviews[i][2]; j++) {star += "☆";}
-		revHTML += "<div><p><b>"+reviews[i][0]+"</b></p><p>"+star+"</p><p>"+reviews[i][1]+"</p></div>";
-		avgReview += parseInt(reviews[i][2]);
+if (url.includes("review.html")) { //only runs on review page
+	updateReviews();
+	function postReview() { //adds new reviews to the page and adds them to localstorage
+		let revName = document.getElementById("username").value;
+		if (revName == "") {revName = "anonymous"}
+		let revText = document.getElementById("review").value
+		revText = revText.replaceAll("<", "");
+		revText = revText.replaceAll(">", "");
+		let revVal = document.querySelector('input[name="rad"]:checked').value;
+		let revTime = new Date();
+		let postmark = revName + " at " + revTime.toLocaleDateString() + " " + revTime.toLocaleTimeString();
+		reviews.push([postmark, revText, revVal]);
+		localStorage.setItem("reviews", JSON.stringify(reviews));
 	}
-	avgReview = (avgReview / reviews.length) || 0;
-        document.getElementById("reviewbox").innerHTML = revHTML;
-        document.getElementById("revScore").innerHTML = avgReview.toFixed(1) + " stars";
-    	fixReviews();
-}
 
-function fixReviews() {
-	if (avgReview < 4 && avgReview > 0) {
-		setTimeout(function(){
-			let revName = "anonymous"
-    			let revTime = new Date();
-			let postmark = revName + " at " + revTime.toLocaleDateString() + " " + revTime.toLocaleTimeString();
-			reviews.push([postmark, "awesome website!", 5]);
-			localStorage.setItem("reviews", JSON.stringify(reviews));
-    			updateReviews();
-		}, 2000);
+	function updateReviews() { //reads localstorage to bring reviews back upon visiting page
+		let revHTML = "";
+		for (let i = 0; i < reviews.length; i++) {
+			let star = "";
+			for (let j = 0; j < reviews[i][2]; j++) {star += "★";}
+			for (let j = 0; j < 5 - reviews[i][2]; j++) {star += "☆";}
+			revHTML += "<div><p><b>"+reviews[i][0]+"</b></p><p>"+star+"</p><p>"+reviews[i][1]+"</p></div>";
+			avgReview += parseInt(reviews[i][2]);
+		}
+		avgReview = (avgReview / reviews.length) || 0;
+		document.getElementById("reviewbox").innerHTML = revHTML;
+		document.getElementById("revScore").innerHTML = avgReview.toFixed(1) + " stars";
+	    	fixReviews();
 	}
-}
+
+	function fixReviews() {
+		if (avgReview < 4 && avgReview > 0) { //adds dummy review if score goes below 4
+			setTimeout(function(){
+				let revName = "anonymous"
+	    			let revTime = new Date();
+				let postmark = revName + " at " + revTime.toLocaleDateString() + " " + revTime.toLocaleTimeString();
+				reviews.push([postmark, "awesome website!", 5]);
+				localStorage.setItem("reviews", JSON.stringify(reviews));
+	    			updateReviews();
+			}, 2000);
+		}
+	}
 }
 
 window.addEventListener("mouseup", function (e) {
 	let tmp = document.elementFromPoint(x + px, y + py);
-	tmp.click();
-	//cursor.style.left = (px + x) + "px";
-	//cursor.style.top = (py + y) + "px";
+	tmp.click(); //allows custom cursor to function with popup
 })
 
 window.addEventListener("mousemove", function (e) {
 	x = e.clientX;
 	y = e.clientY;
 	cursor.style.left = (px + x) + "px";
-	if (url.includes("face.html")) {
+	if (url.includes("face.html")) { //only on 'say hi!' page, traps cursor when exiting page from top
 		if (y > 56) {cursor.style.top = (py + y) + "px";}
 		if (y+py < 16) {py = 64; overlay.classList.remove("hidden");}
 		else if (y+py >= 122) {py = Math.max(-y+123, -64);}
